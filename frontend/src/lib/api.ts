bbe148c8c931fd70813cd509e263206925ebb4ff
Marcel Müller <marcel@bazzite.fritz.box>
2026-05-01 22:35:08 +0200

    Added Linkage

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,1 +1,7 @@
-export const API_URL = typeof window !== "undefined" ? `${API_URL}` : "http://backend:8000";
+// Use a different variable for server-side to avoid conflict if ever run in same context
+const serverApiUrl = "http://backend:8000"; 
+
+export const API_URL = typeof window !== "undefined" 
+    ? `http://${window.location.hostname}:8000` 
+    : serverApiUrl;
+
